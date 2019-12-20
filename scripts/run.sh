c17baf0fcbf75c27e4f8aa240a11eb40ef103a9b
j-philippe.lingrand <jean-philippe.lingrand@ssense.com>
2019-12-20 16:09:35 -0500

    TK6 Add comments

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,8 @@
+# Get the postman collection
 wget -O postman.request $1
 
+# Convert the postman collection to k6 script
 node node_modules/postman-to-k6/bin/postman-to-k6.js -o k6-script.js postman.request
 
+# Run k6 command with script
 k6 run --duration $3 --vus $5 k6-script.js
